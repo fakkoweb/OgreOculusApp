15759118eb477deb0f7ff8f4aff6d68ed86f7aca
Dario Facchini <fakkoweb@libero.it>
2015-08-31 11:40:29 +0200

    Shaders materials and images updated to support aspect-ratio. Mesh updated so that its central vertex has (0,0) in uv map (thus making it simpler to understand)

--- media/FisheyeImageDistortionFragment.glsl
+++ media/FisheyeImageDistortionFragment.glsl
@@ -1,19 +1,37 @@
 // GLSL Pixel shader definition
 
 
-// Texture sampler (used to access single textels from a texture)
+// GLSL Texture sampler (used to access single textels from a texture)
 uniform sampler2D currentTexture;
 
 // Load in values defined in the material:
-uniform float adjustTextureScale;   // Higher value = smaller coords = wider texture
-uniform vec2 adjustTextureOffset;   // Texture recenter
+uniform float adjustTextureAspectRatio;	// Higher value = smaller X coords = wider texture on X value
+uniform float adjustTextureScale;   	// Higher value = smaller coords = wider texture
+uniform vec2 adjustTextureOffset;   	// Texture recenter
 
 void main(void)
 {
-    // Scale and apply offset to uv map, then sample and apply color
+	// These are UV coordinates of the UV circle center, which corresponds to
+	// the vertex of the sphere in the middle (right in front of the user),
+	// in respect to which scaling should take place.
+	// This value is useful to make calibration more user-friendly.
+	// 
+	// In this specific case, sphere mesh has been modeled so that UV coordinates
+	// in this vertex are (0,0). This formula has been found experimentally,
+	// since Ogre probably interprets coords in its own way.
+	adjustTextureOffset.x = -adjustTextureOffset.x;
+	vec2 uvScaleCenter = vec2(adjustTextureOffset.x,1-(-adjustTextureOffset.y));
+
+	vec2 AlteredTexCoord;
+	// apply offset
+	AlteredTexCoord = gl_TexCoord[0].xy + adjustTextureOffset;
+	// apply centered ratio (only to x) and scale
+	AlteredTexCoord.x = ((AlteredTexCoord.x - uvScaleCenter.x) * 1/adjustTextureAspectRatio * 1/adjustTextureScale) + uvScaleCenter.x;
+	AlteredTexCoord.y = ((AlteredTexCoord.y - uvScaleCenter.y) * 1/adjustTextureScale) + uvScaleCenter.y;
+    // apply color using altered uv map
     gl_FragColor = texture2D
     (
         currentTexture,
-        gl_TexCoord[0].xy * 1/adjustTextureScale - adjustTextureOffset
+        AlteredTexCoord.xy
     );
 }
